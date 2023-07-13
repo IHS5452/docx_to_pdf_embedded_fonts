71010f5b205ab10e14ad717349d31090f1c46c82
Ian Schrauth <ianschrauth@sbcglobal.net>
2023-07-13 12:01:46 -0500

    Update docx to pdf.py

--- docx to pdf.py
+++ docx to pdf.py
@@ -1,16 +1,21 @@
-from docx2pdf import convert
 import os
+import win32com.client
 
 def convert_to_pdf_with_embedded_fonts(save_location, output_filename, docx_file):
     output_filepath = os.path.join(save_location, output_filename)
     
     try:
-        convert(docx_file, output_filepath, keep_active=False, silent=True, flags="--embed-all-fonts")
+        word = win32com.client.Dispatch("Word.Application")
+        doc = word.Documents.Open(docx_file)
+        doc.SaveAs(output_filepath, FileFormat=17)
+        doc.Close()
+        word.Quit()
+        
         print("Conversion to PDF completed successfully. Output file:", output_filepath)
     except Exception as e:
         print("Error converting to PDF:", str(e))
 
-# Usage example
+
 save_location = input("Enter the save location: ").strip('\'"')
 output_filename = input("Enter the output file name: ").strip('\'"')
 docx_file = input("Enter the path to the DOCX file: ").strip('\'"')
